20e7c14416d02b327cd31dfffa8b5cff9ed0f57b
Rozvita Lilly Štiglinc <117945803+19Lilly@users.noreply.github.com>
2023-03-07 22:52:43 +0100

    basic design without functions

---
+++ index.js
@@ -0,0 +1,16 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+let convertBtn = document.getElementById("convert-btn")
+console.log(convertBtn)
+convertBtn.addEventListener("click", function(){
+    console.log("clicked")
+})
+
+// function convert(inputValue) {
+//     if 
+//  iputValue * const1 | iputValue / const1   
+// }
