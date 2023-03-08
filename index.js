85f2efb34ac5dbee0d0156673f6ddfd930259452
Rozvita Lilly Štiglinc <117945803+19Lilly@users.noreply.github.com>
2023-03-08 21:22:13 +0100

    function complete

--- index.js
+++ index.js
@@ -4,13 +4,41 @@
 1 kilogram = 2.204 pound
 */
 
-let convertBtn = document.getElementById("convert-btn")
-console.log(convertBtn)
-convertBtn.addEventListener("click", function(){
+const convertBtn = document.getElementById("convert-btn")
+
+
+let length = document.getElementById("length-result")
+let volume = document.getElementById("volume-result")
+let mass = document.getElementById("mass-result")
+
+convertBtn.addEventListener("click", function convert(){
+    const userInput = document.getElementById("input-value").value
     console.log("clicked")
+    console.log(userInput)
+    
+    const meterFeet = 3.281
+    const literGallon = 0.264
+    const kilogramPound = 2.204
+
+    let meters = ((Number(userInput)/meterFeet).toFixed(3))
+    let feets = ((Number(userInput)*meterFeet).toFixed(3))
+
+    let liters = ((Number(userInput)/literGallon).toFixed(3))
+    let gallons = ((Number(userInput)*literGallon).toFixed(3))
+
+    let kilograms = ((Number(userInput)/kilogramPound).toFixed(3))
+    let pounds = ((Number(userInput)*kilogramPound).toFixed(3))
+
+    length.innerHTML = `${userInput} meters = ${feets} feet | ${userInput} feet = ${meters} meters`
+    volume.innerHTML = `${userInput} liters = ${gallons} gallons | ${userInput} gallons = ${liters} liters`
+    mass.innerHTML = `${userInput} kilos = ${pounds} pounds | ${userInput} pounds = ${kilograms} kilos`
 })
 
 // function convert(inputValue) {
 //     if 
 //  iputValue * const1 | iputValue / const1   
 // }
+
+// 0 meters = 0 feet | 0 feet = 0 meters
+// 0 liters = 0 gallons | 0 gallons = 0 liters
+// 0 kilos = 0 pounds | 0 pounds = 0 kilos
